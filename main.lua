af65afbdacc47db4fbb89f8fd9719210d6145322
SimonLarsen <simonhffh@gmail.com>
2011-07-27 20:56:10 +0200

    Moved stuff to defines- and utils.lua. Added bass matrix

--- main.lua
+++ main.lua
@@ -1,25 +1,8 @@
-freq = {1.682, 1.414, 1.2599, 1.1246, 0.944, 0.8409, 0.7071, 0.62996,
-		0.561228, 0.4719, 0.4204, 0.3534, 0.31499, 0.2806, 0.2360, 0.2102}
-
-WAIT = 0.200
-
-CELLW,CELLH = 16,16
-
-PIANO_OFF_X = 2
-PIANO_OFF_Y = 1
-KICK_OFF_X = 2
-KICK_OFF_Y = 18
-SNARE_OFF_X = 2
-SNARE_OFF_Y = 19
-HAT_OFF_X = 2
-HAT_OFF_Y = 20
-RIDE_OFF_X = 2
-RIDE_OFF_Y = 21
-
-bgcolor = {54,110,212}
+require("defines")
+require("utils")
 
 function love.load()
-	love.graphics.setMode(19*CELLW,23*CELLW)
+	love.graphics.setMode(37*CELLW,23*CELLW)
 	love.graphics.setBackgroundColor(bgcolor)
 	love.graphics.setLineWidth(2)
 	math.randomseed(os.time())
@@ -27,19 +10,24 @@ function love.load()
 
 	update = 0
 	play_x = 0
+	wait = 0.42
 end
 
 function love.update(dt)
 	-- play
 	update=update+dt
-	if update > WAIT then
-		update = update%WAIT
+	if update > wait then
+		update = update%wait
 		play_x = (play_x+1)%16
-		-- play piano
+		-- play piano and bass
 		for iy=0,15 do
 			if matPiano[play_x+iy*16] == 1 then
 				if pianoTone[iy+1]:isStopped() == false then pianoTone[iy+1]:stop() end
 				pianoTone[iy+1]:play()
+			end
+			if matBass[play_x+iy*16] == 1 then
+				if bassTone[iy+1]:isStopped() == false then bassTone[iy+1]:stop() end
+				bassTone[iy+1]:play()
 			end
 		end
 		if matKick[play_x] == 1 then
@@ -85,6 +73,14 @@ function love.update(dt)
 			elseif my >= RIDE_OFF_Y*CELLH and my < (RIDE_OFF_Y+1)*CELLH then
 				matRide[cx] = val
 			end
+		-- check second column
+		elseif mx >= BASS_OFF_X*CELLW and mx < (BASS_OFF_X+16)*CELLW then
+			-- bass
+			local cx = math.floor((mx-BASS_OFF_X*CELLW)/CELLW)
+			if my >= BASS_OFF_Y*CELLH and my < (BASS_OFF_Y+16)*CELLH then
+				local cy = math.floor((my-BASS_OFF_Y*CELLH)/CELLH)
+				matBass[cx+cy*16] = val
+			end
 		end
 	end
 end
@@ -92,84 +88,59 @@ end
 function love.draw()
 	love.graphics.setColor(255,255,255,255)
 	-- draw icons
-	love.graphics.drawq(imgTiles,quad[1],CELLW,PIANO_OFF_Y*CELLH)
-	love.graphics.drawq(imgTiles,quad[0],CELLW,KICK_OFF_Y*CELLH)
-	love.graphics.drawq(imgTiles,quad[2],CELLW,SNARE_OFF_Y*CELLH)
-	love.graphics.drawq(imgTiles,quad[12],CELLW,HAT_OFF_Y*CELLH)
-	love.graphics.drawq(imgTiles,quad[3],CELLW,RIDE_OFF_Y*CELLH)
+	love.graphics.drawq(imgTiles,quad[0],CELLW,PIANO_OFF_Y*CELLH)
+	love.graphics.drawq(imgTiles,quad[8],CELLW,KICK_OFF_Y*CELLH)
+	love.graphics.drawq(imgTiles,quad[16],CELLW,SNARE_OFF_Y*CELLH)
+	love.graphics.drawq(imgTiles,quad[24],CELLW,HAT_OFF_Y*CELLH)
+	love.graphics.drawq(imgTiles,quad[32],CELLW,RIDE_OFF_Y*CELLH)
+	love.graphics.drawq(imgTiles,quad[40],(BASS_OFF_X-1)*CELLW,BASS_OFF_Y*CELLH)
 
 	love.graphics.push()
 	-- draw piano matrix
 	love.graphics.translate(PIANO_OFF_X*CELLW,PIANO_OFF_Y*CELLH)
 	for iy=0,15 do
 		for ix=0,15 do
-			love.graphics.drawq(imgTiles,quad[6+matPiano[ix+iy*16]],ix*CELLW,iy*CELLH)
+			if ix == play_x then
+				love.graphics.drawq(imgTiles,quad[18+matPiano[ix+iy*16]],ix*CELLW,iy*CELLH)
+			else
+				love.graphics.drawq(imgTiles,quad[17+matPiano[ix+iy*16]],ix*CELLW,iy*CELLH)
+			end
 		end
 	end
-	love.graphics.setColor(0,0,0,255)
-	local lx = (play_x+update/WAIT)*CELLW
-	love.graphics.line(lx,0,lx,16*CELLH)
+	love.graphics.pop()
+	-- draw bass matrix
+	love.graphics.push()
+	love.graphics.translate(BASS_OFF_X*CELLW,BASS_OFF_Y*CELLH)
+	for iy=0,15 do
+		for ix=0,15 do
+			if ix == play_x then
+				love.graphics.drawq(imgTiles,quad[26+matBass[ix+iy*16]],ix*CELLW,iy*CELLH)
+			else
+				love.graphics.drawq(imgTiles,quad[25+matBass[ix+iy*16]],ix*CELLW,iy*CELLH)
+			end
+		end
+	end
 	love.graphics.pop()
 	-- draw drum matrices
 	love.graphics.setColor(255,255,255,255)
 	love.graphics.translate(KICK_OFF_X*CELLW,KICK_OFF_Y*CELLH)
 	for x=0,15 do
-		love.graphics.drawq(imgTiles,quad[10+matKick[x]],x*CELLW,0)
-		love.graphics.drawq(imgTiles,quad[4+matSnare[x]],x*CELLW,CELLH)
-		love.graphics.drawq(imgTiles,quad[8+matHat[x]],x*CELLW,2*CELLH)
-		love.graphics.drawq(imgTiles,quad[14+matRide[x]],x*CELLW,3*CELLH)
+		if x == play_x then
+			love.graphics.drawq(imgTiles,quad[2+matKick[x]],x*CELLW,0)
+			love.graphics.drawq(imgTiles,quad[10+matSnare[x]],x*CELLW,CELLH)
+			love.graphics.drawq(imgTiles,quad[26+matHat[x]],x*CELLW,2*CELLH)
+			love.graphics.drawq(imgTiles,quad[18+matRide[x]],x*CELLW,3*CELLH)
+		else
+			love.graphics.drawq(imgTiles,quad[1+matKick[x]],x*CELLW,0)
+			love.graphics.drawq(imgTiles,quad[9+matSnare[x]],x*CELLW,CELLH)
+			love.graphics.drawq(imgTiles,quad[25+matHat[x]],x*CELLW,2*CELLH)
+			love.graphics.drawq(imgTiles,quad[17+matRide[x]],x*CELLW,3*CELLH)
+		end
 	end
 end
 
 function love.keypressed(k)
 	if k == ' ' then
-		for iy=0,15 do
-			for ix=0,15 do
-				matPiano[ix+iy*16] = 0
-			end
-			matKick[iy] = 0
-			matSnare[iy] = 0
-			matHat[iy] = 0
-			matRide[iy] = 0
-		end
-	end
-end
-
-function loadResources()
-	imgTiles = love.graphics.newImage("res/tiles.png")
-	imgTiles:setFilter("nearest","nearest")
-	quad = {}
-	for iy=0,3 do
-		for ix=0,3 do
-			quad[ix+iy*4] = love.graphics.newQuad(ix*CELLW,iy*CELLH,CELLW,CELLH,imgTiles:getWidth(),imgTiles:getHeight())
-		end
-	end
-
-	pianoData = love.sound.newSoundData("res/piano.ogg")
-	pianoTone = {}
-	for i=1,#freq do
-		pianoTone[i] = love.audio.newSource(pianoData)
-		pianoTone[i]:setPitch(freq[i])
-	end
-	matPiano = {}
-	for iy=0,15 do
-		for ix=0,15 do
-			matPiano[ix+iy*16] = 0
-		end
-	end
-
-	sndKick = love.audio.newSource("res/kick.ogg","static")
-	sndSnare = love.audio.newSource("res/snare.ogg","static")
-	sndHat = love.audio.newSource("res/hat.ogg","static")
-	sndRide = love.audio.newSource("res/ride.ogg","static")
-	matKick = {}
-	matSnare = {}
-	matHat = {}
-	matRide = {}
-	for x=0,15 do
-		matKick[x] = 0
-		matSnare[x] = 0
-		matHat[x] = 0
-		matRide[x] = 0
+		clearPatterns()
 	end
 end
---
+++ utils.lua
@@ -0,0 +1,50 @@
+function loadResources()
+	imgTiles = love.graphics.newImage("res/tiles.png")
+	imgTiles:setFilter("nearest","nearest")
+	createQuads()
+
+	pianoData = love.sound.newSoundData("res/piano.ogg")
+	bassData = love.sound.newSoundData("res/bass.ogg")
+	pianoTone = {}
+	bassTone = {}
+	for i=1,#freq do
+		pianoTone[i] = love.audio.newSource(pianoData)
+		bassTone[i] = love.audio.newSource(bassData)
+		pianoTone[i]:setPitch(freq[i])
+		bassTone[i]:setPitch(freq[i])
+	end
+	sndKick = love.audio.newSource("res/kick.ogg","static")
+	sndSnare = love.audio.newSource("res/snare.ogg","static")
+	sndHat = love.audio.newSource("res/hat.ogg","static")
+	sndRide = love.audio.newSource("res/ride.ogg","static")
+	matPiano = {}
+	matKick = {}
+	matSnare = {}
+	matHat = {}
+	matRide = {}
+	matBass = {}
+	clearPatterns()
+end
+
+function clearPatterns()
+	for iy=0,15 do
+		for ix=0,15 do
+			matPiano[ix+iy*16] = 0
+			matBass[ix+iy*16] = 0
+		end
+		matKick[iy] = 0
+		matSnare[iy] = 0
+		matHat[iy] = 0
+		matRide[iy] = 0
+		matBass[iy] = 0
+	end
+end
+
+function createQuads()
+	quad = {}
+	for iy=0,7 do
+		for ix=0,7 do
+			quad[ix+iy*8] = love.graphics.newQuad(ix*CELLW,iy*CELLH,CELLW,CELLH,imgTiles:getWidth(),imgTiles:getHeight())
+		end
+	end
+end
